28507ff6f4dd4b1c2669e108260580119ad6018b
Vincent Uden <vincentuden@gmail.com>
2022-12-01 23:35:43 +0100

    Implement menu interactivity

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,7 +1,19 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 const Menu = () => {
-  const [open, setOpen] = useState<boolean>(false);
+  const [open, setOpen] = useState<boolean>(true);
+  const [discord, setDiscord] = useState<boolean>(false);
+  const [email, setEmail] = useState<boolean>(false);
+
+  const popupRef = useRef<HTMLDivElement | null>(null);
+
+  const openPopup = () => {
+    const popup = popupRef.current!!;
+
+    popup.classList.add("show-popup");
+
+    setTimeout(() => {popup.classList.remove("show-popup")}, 2000);
+  }
 
   return (
     <div className="fixed top-0 left-0 h-full">
@@ -34,24 +46,64 @@ const Menu = () => {
             </a>
           </div>
           <div className="absolute bottom-24 left-24">
-            <div className="i-mdi-github w-32 h-32 text-slate-dark inline-block"></div>
-            <div className="i-carbon-logo-discord w-32 h-32 text-slate-dark inline-block mx-8"></div>
-            <div className="i-mdi-alternate-email w-32 h-32 text-slate-dark inline-block"></div>
+            <a href="https://github.com/vincent-uden">
+              <div className="i-mdi-github w-32 h-32 text-slate-dark inline-block @hover-text-slate-blue transition-colors"></div>
+            </a>
+            <div
+              className="inline-block relative mx-8"
+              onMouseEnter={() => setDiscord(true)}
+              onMouseLeave={() => setDiscord(false)}
+              onClick={() => {navigator.clipboard.writeText("Vincent Udén#4873"); openPopup()}}
+            >
+              <div className="i-carbon-logo-discord w-32 h-32 text-slate-dark inline-block @hover-text-slate-blue transition-colors"></div>
+              <div
+                className={`absolute bottom-full left-0 pointer-events-none ${
+                  discord ? "opacity-100" : "opacity-0"
+                } transition-opacity`}
+              >
+                <p className="text-pale-grey bg-slate-dark py-4 rounded-4 text-lg font-gothic whitespace-nowrap relative w-52 -left-10 text-center">
+                  Vincent Udén#4873
+                </p>
+              </div>
+            </div>
+            <div
+              className="inline-block relative"
+              onMouseEnter={() => setEmail(true)}
+              onMouseLeave={() => setEmail(false)}
+              onClick={() => {navigator.clipboard.writeText("vincentuden@gmail.com"); openPopup()}}
+            >
+              <div className="i-mdi-alternate-email w-32 h-32 text-slate-dark inline-block @hover-text-slate-blue transition-colors"></div>
+              <div
+                className={`absolute bottom-full left-0 pointer-events-none z-40 ${
+                  email ? "opacity-100" : "opacity-0"
+                } transition-opacity`}
+              >
+                <p className="text-pale-grey bg-slate-dark py-4 rounded-4 text-lg font-gothic whitespace-nowrap relative w-60 -left-10 text-center">
+                  vincentuden@gmail.com
+                </p>
+              </div>
+            </div>
           </div>
         </div>
         <div className="translate-x-full w-fit h-full overflow-x-visible shadow-menu">
           <div className="bg-pale-grey w-58 h-full inline-block z-20" />
           <div className="bg-slate-dark w-32 h-full inline-block z-20" />
-          <div className="bg-slate-blue w-24 h-full inline-block z-20">
+          <div
+            className="bg-slate-blue w-24 h-full inline-block z-20"
+            onClick={() => setOpen(!open)}
+          >
             <div
-              className={`i-mdi-chevron-double-left bg-pale-grey w-24 h-full m-auto scale-x-75 ${
+              className={`i-bi-chevron-double-left text-pale-grey w-16 h-full m-auto scale-x-100 ${
                 open ? "rotate-0" : "rotate-180"
               } transition-transform`}
-              onClick={() => setOpen(!open)}
             />
           </div>
         </div>
       </nav>
+
+      <div className="fixed left-50vw bottom-10 z-40 opacity-0" ref={popupRef}>
+        <p className="-translate-x-1/2 bg-slate-dark shadow-popup text-pale-grey p-4 text-xl rounded-4">Copied to clipboard</p>
+      </div>
     </div>
   );
 };
