a27ee1b8bf8c05aeec249979a10903b817367f1b
Vincent Uden <vincentuden@gmail.com>
2023-08-31 00:10:13 +0200

    Uno sucks ass. Move to tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,50 +1,11 @@
 import { defineConfig } from "astro/config";
 
-import Unocss from "unocss/vite";
-import { presetWebFonts, presetWind, presetMini, presetIcons, transformerDirectives } from "unocss";
-import { presetScrollbar } from "unocss-preset-scrollbar";
+import UnoCSS from "unocss/astro";
 
 // https://astro.build/config
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
-  vite: {
-    plugins: [
-      Unocss({
-        theme: {
-          colors: {
-            onyx: "#383a3e",
-            xanadu: "#808F85",
-            eton: "#91C499",
-            linen: "#F2E9DC",
-            coral: "#FE6D73",
-            paleGrey: "#DCE8F2",
-            slateBlue: "#8C9DBF",
-            slateDark: "#5B647B",
-            darkGrey: "#383A3E",
-            darkerGrey: "#202125bb",
-          },
-        },
-        presets: [
-          presetMini({}),
-          presetWind({}),
-          presetIcons({}),
-          presetWebFonts({
-            provider: "google",
-            fonts: {
-              sans: "Roboto:100,200,300,400,500,600,700,800,900",
-              geo: ["Geometos", "Roboto"],
-              gothic: ["Century Gothic", "Roboto"],
-            },
-          }),
-          presetScrollbar({}),
-        ],
-        transformers: [
-            transformerDirectives(),
-        ],
-      }),
-    ],
-  },
+    integrations: [react(), UnoCSS({injectReset: true})],
 });
