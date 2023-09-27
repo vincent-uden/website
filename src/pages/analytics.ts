85537cde98f8cf2113e11bd89fedc90a329286c7
Vincent Uden <vincentuden@gmail.com>
2023-09-27 23:09:45 +0200

    Move analytics to be non-blocking for page render

---
+++ src/pages/analytics.ts
@@ -0,0 +1,25 @@
+import type { APIContext } from "astro";
+
+export async function POST({ request }: APIContext) {
+  console.log("ANALYTICS");
+
+  const requestBody = await request.json();
+  console.log(requestBody);
+
+  let body = {
+    path: requestBody.path ?? "/unknown",
+    projectId: import.meta.env.ANALYTICS_PROJECT_ID,
+    secret: import.meta.env.ANALYTICS_PROJECT_SECRET,
+  };
+
+  const resp = await fetch(import.meta.env.ANALYTICS_URL, {
+    method: "POST",
+    cache: "no-cache",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+
+  return new Response(JSON.stringify({ success: resp.ok }));
+}
