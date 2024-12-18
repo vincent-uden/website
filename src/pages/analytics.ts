690c21e3a810952fead1d80dfb4a29df13992a5b
Vincent Udén <vincent.uden@colmeo.se>
2024-12-19 00:11:40 +0100

    Update bio

--- src/pages/analytics.ts
+++ src/pages/analytics.ts
@@ -10,14 +10,18 @@ export async function POST({ request }: APIContext) {
     secret: import.meta.env.ANALYTICS_PROJECT_SECRET,
   };
 
-  const resp = await fetch(import.meta.env.ANALYTICS_URL, {
-    method: "POST",
-    cache: "no-cache",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(body),
-  });
+  try {
+    const resp = await fetch(import.meta.env.ANALYTICS_URL, {
+      method: "POST",
+      cache: "no-cache",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
 
-  return new Response(JSON.stringify({ success: resp.ok }));
+    return new Response(JSON.stringify({ success: resp.ok }));
+  } catch (err) {
+    return new Response(JSON.stringify({ error: err.message }));
+  }
 }
